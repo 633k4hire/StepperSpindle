74dcdeac76b78d7a025cda257153fb6c369d422f
Matthew Metzger <metzger.matthew@gmail.com>
2025-03-23 12:40:17 -0400

    new interface and pin defs

--- src/pins.h
+++ src/pins.h
@@ -2,12 +2,12 @@
 
  //seeduino xiao esp32c3 https://wiki.seeedstudio.com/XIAO_ESP32C3_Getting_Started/
  // GPIO pin configuration for inputs
-    #define  dirPinIn 20       // Direction input pin
+    #define  dirPinIn 7       // Direction input pin
     #define enablePinIn 8    // Enable input pin
     #define stepPinIn 9      // Step input pin
     #define pwmInPin 10       // PWM input pin for spindle speed control
 
     // GPIO pin configuration for outputs
-    #define dirPinOut 21    // Direction output pin to stepper
-    #define enablePinOut 7   // Enable output pin to stepper
-    #define stepPinOut 6     // Step output pin to stepper
+    #define dirPinOut 6    // Direction output pin to stepper
+    #define enablePinOut 5   // Enable output pin to stepper
+    #define stepPinOut 4     // Step output pin to stepper
